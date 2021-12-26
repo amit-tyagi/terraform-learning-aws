a0f6dea250fd3f60471f7afb8283ec17772ac231
Tyagi, Amit <iamittyagi@gmail.com>
2021-12-26 13:33:34 -0600

    Adding provider.tf file

--- provider.tf
+++ provider.tf
@@ -1,13 +1,28 @@
+
 terraform {
+  
   required_providers {
     aws = {
       source = "hashicorp/aws"
       version = "3.70.0"
     }
   }
+
+  backend "s3" {
+    
+    bucket            = "terraform-configuration-atyagi"
+    key               = "state/terraform.tfstate"
+    region            = "us-east-1"
+    profile           = "terraform-admin"
+    dynamodb_table    = "terraform-state-lock-table"
+
+  }
+
 }
 
 provider "aws" {
+
   region = "us-east-1"
   profile = "terraform-admin"
+
 }
