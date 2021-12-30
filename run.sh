3b79f1e7ce26044e31d29d5ff6ce942783c7beaf
Amit Tyagi <iamittyagi@gmail.com>
2021-12-30 09:35:53 -0600

    spelling correction

--- run.sh
+++ run.sh
@@ -8,7 +8,7 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 # Always run from the location of this script
 cd $DIR
 
-if [ $# -gt 0 ]; then # this means if number of agruements passed are greater than zero
+if [ $# -gt 0 ]; then # this means if number of arguments passed are greater than zero
   if [ "$2" == "init" ]; then # $2 = init, plan, apply, etc.
     terraform -chdir=./$TF_ENV init --backend-config ../backend-$TF_ENV.tf
   else
